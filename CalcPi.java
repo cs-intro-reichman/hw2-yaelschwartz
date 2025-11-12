bbe704a6ba4e054b8ee6f579be5de73990b8a132
Yael Schwartz <yaelschwartz@Yaels-MacBook-Air.local>
2025-11-12 14:47:51 +0200

    Completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,16 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
-	}
+    int num = Integer.parseInt(args[0]);
+
+	   System.out.println("pi according to Java: " + Math.PI);
+	   
+	double sum = 0.0;
+
+	for (int i = 0; i < num; i++) {
+		double term = Math.pow (-1, i) / (2 * i + 1);
+		sum += term;
+    	}
+        System.out.println("pi, approximated: " + sum * 4); 
+    }
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,34 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+	public static void main(String args[]) 
+	{
+	    int seed = Integer.parseInt(args[0]);
+        String mode = args[1];
+		
+		if (mode.equals("v"))
+		{
+			for (int i = 1; i <= seed; i++) 
+			{
+				int x = i;
+				int steps = 1;
+				System.out.print(x + " ");
+
+				while (x != 1) 
+				{
+					if (x % 2 == 0 ) 
+					{
+						x = x / 2;
+					} else 
+					{
+						x = x * 3 + 1;
+					} System.out.print(x + " ");
+					steps++;
+				}
+			
+				System.out.println("(" + steps + ")");
+            } 	
+		}
+			System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");	
+    }
 }
+ 
